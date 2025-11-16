0a4ed65255e4ffe98d043c9c50ac2c4aac8e1019
Chris <Chris@ChristianPC>
2025-11-16 02:35:09 -0600

    project

---
+++ project/src/project.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	string firstName = "Peter";
+	string lastName = "Parker";
+	string streetAddress = "123 Mary Jane Drive";
+	string city = "New York";
+	string zipCode = "2048";
+
+
+	cout << "First Name: " <<firstName<< endl; // num value
+	cout << "Last Name: " <<lastName<< endl; // deci value
+	cout << "Street Address: " <<streetAddress<< endl; // charecter value
+	cout << "City: " <<city<< endl; // b value
+	cout << "Zip Code: " <<zipCode<< endl; // b value
+
+	return 0;
+}
